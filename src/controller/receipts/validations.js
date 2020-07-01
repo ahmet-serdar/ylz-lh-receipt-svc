a8a10d055f94c66efc33568f4731ba17cb5f4253
ahmet-serdar <baykalaserdar@gmail.com>
2020-07-01 21:45:32 +0100

    changes

--- src/controller/receipts/validations.js
+++ src/controller/receipts/validations.js
@@ -11,31 +11,89 @@ const validations = Object.freeze({
       errorMessage: 'Wrong format!',
     },
   },
-  firstName: {
-    in: ['body'],
-    exists: {
-      errorMessage: 'First name is required',
-    }
-  },
-  lastName: {
-    exists: {
-      errorMessage: 'Last name is required',
-    }
-  },
-  isIndividual: { 
-    isBoolean: {optional: { options: { nullable: true }}},
-    errorMessage: 'Individual must be true or false!'      
-  },
-  phones: {
-    optional: { options: { nullable: true } },
-    errorMessage: 'phone'
-  },
-  email: {
-    optional: { options: { nullable: true } },
-    isEmail: true
-    // errorMessage: 'It must be email'    
-  },
-  createdBy: {},
+  amount(locationType = constants.HttpRequestLocation.query, isRequired = true) {
+    return {
+      in: [locationType],
+      optional: !isRequired,
+      custom: {
+        options: (value) => {
+          if(!isNaN(value)) {
+            return value >= 0}
+        },
+        errorMessage: `Amount cannot be less then 0!`
+      }
+  }},
+  amountInLetters(locationType = constants.HttpRequestLocation.query, isRequired = true) {
+    return {
+      in: [locationType],
+      optional: !isRequired,
+      custom: {
+        options: (value) => {
+          if(value) {
+            return value.length >= 1}
+        },
+        errorMessage: `Please write the ammount in letters!`
+      }
+  }},
+  date(locationType = constants.HttpRequestLocation.query, isRequired = true) {
+    return {
+      in: [locationType],
+      optional: !isRequired,
+      custom: {
+        options: (value, format = null) => {
+            try {
+              if (isNullOrUndefined(format)) {
+                return isNaN(new Date(value).getTime()) ? false : true;
+              } else {
+                // TODO: Coming soon.
+                const parms = value.split(/[\-\/]/);
+                const yyyy = parseInt(parms[2], 10);
+                const mm = parseInt(parms[1], 10);
+                const dd = parseInt(parms[0], 10);
+                const date = new Date(yyyy, mm - 1, dd, 0, 0, 0, 0);
+          
+                return mm === date.getMonth() + 1 && dd === date.getDate() && yyyy === date.getFullYear();
+              }
+            } catch (err) {
+              return false;
+            }
+        },
+        errorMessage: `Date is invalid!`
+      }
+  }},
+  receivedBy(locationType = constants.HttpRequestLocation.query, isRequired = true) {
+    return {
+      in: [locationType],
+      optional: !isRequired,
+        errorMessage: `Received by is required!`
+      
+  }},
+  paymentType(locationType = constants.HttpRequestLocation.query, isRequired = true) {
+    return {
+      in: [locationType],
+      optional: !isRequired,
+        errorMessage: `Payment type by is required!`
+      
+  }},
+  paymentReason(locationType = constants.HttpRequestLocation.query, isRequired = true) {
+    return {
+      in: [locationType],
+      optional: !isRequired,
+        errorMessage: `Payment reason by is required!`
+      
+  }},
+  // amountInLetters(locationType = constants.HttpRequestLocation.query, isRequired = true) {
+  //   return {
+  //     in: [locationType],
+  //     optional: !isRequired,
+  //     custom: {
+  //       options: (value) => {
+  //         if(value) {
+  //           return value.length >= 1}
+  //       },
+  //       errorMessage: `Please write the ammount in letters!`
+  //     }
+  // }},
 });
 
 /*
@@ -63,12 +121,17 @@ const validator = Object.freeze({
     id: validations.id,
   },
   create: {
+    amount: validations.amount('body'),
+    amountInLetters: validations.amountInLetters('body'),
+    receivedBy: validations.receivedBy('body'),
+    paymentType: validations.paymentType('body'),
+    paymentReason: validations.paymentReason('body')
+
   },
   update: {
-  },
-  delete: {
-    id: validations.id,
-  },
+    amount: validations.amount('body', false),
+    amountInLetters: validations.amountInLetters('body', false),
+  }
 });
 
 module.exports = validator
